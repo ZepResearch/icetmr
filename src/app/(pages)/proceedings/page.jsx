a8364ed316a7bbf7407c43c1a246c93a7ae47c4b
ShardulVanage <vsharadul@gmail.com>
2026-05-20 12:02:07 +0530

    fix: Update title and description in Proceedings page metadata

--- src/app/(pages)/proceedings/page.jsx
+++ src/app/(pages)/proceedings/page.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ProceedingsContent from './Content'
 
 export const metadata = {
-    title: 'Proceedings | ICSIFT',
-    description: 'Browse published conference proceedings from ICSIFT featuring cutting-edge research and scholarly contributions.',
+    title: 'Proceedings ICTMR  | Published Research Papers',
+    description: 'Browse published conference proceedings from ICETMR, featuring cutting-edge research and scholarly contributions.',
 }
 
 function ProceedingsPage() {
